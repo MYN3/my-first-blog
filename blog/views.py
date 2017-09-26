43c505c2bca2716aea5ecd09f99d89e9f026afb5
MYN3 <mynor_mg@hotmail.com>
2017-09-26 11:36:30 -0600

    edite el css y los post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicar
 
 def post_list(request):
     posts = Publicar.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+ post = get_object_or_404(Publicar, pk=pk)
+ return render(request, 'blog/post_detail.html', {'post': post})
